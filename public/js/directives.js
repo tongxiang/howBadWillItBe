efeab79b94b774f968dc894164a514b68d6abba1
Tong Xiang <tong.xiang2@gmail.com>
2014-04-27 21:17:35 -0400

    created d3 angular modules

--- public/js/directives.js
+++ public/js/directives.js
@@ -1,1 +1,13 @@
 'use strict';
+
+angular.module('mean.directives', ['d3'])
+    .directive('d3Bars', ['d3Service', function(d3Service) {
+        return {
+            restrict: 'EA',
+            scope: {},
+            link: function(scope, element, attrs) {
+                d3Service.d3().then(function(d3) {
+                // d3 is the raw d3 object
+                });
+            }}
+    }]);
