50ed795f0d7ca34a33ad5518c6887f711e17b6a9
Tong Xiang <tong.xiang2@gmail.com>
2014-04-29 16:03:43 -0400

    began albersUsa incorporation

--- public/js/directives.js
+++ public/js/directives.js
@@ -1,118 +1,15 @@
 'use strict';
-// angular.module('mean.directives', ['d3'])
-// .directive('d3Bars', ['$window', '$timeout', 'd3Service', 
-//   function($window, $timeout, d3Service) {    
-//     return {
-//       restrict: 'A',
 
-//       scope: {
-//         data: '=',
-//         label: '@',
-//         onClick: '&'
-//       },
-
-//       link: function(scope, ele, attrs) {
-//         console.log('link function running!')
-//         var d3Use = function(d3) {
-//             console.log('d3Use function running')
-//           var renderTimeout;
-//           var margin = parseInt(attrs.margin) || 20,
-//               barHeight = parseInt(attrs.barHeight) || 20,
-//               barPadding = parseInt(attrs.barPadding) || 5;
- 
-//           var svg = d3.select(ele[0])
-//             .append('svg')
-//             .style('width', '100%');
- 
-//           $window.onresize = function() {
-//             scope.$apply();
-//           };
-
-//           scope.data = [
-//               {name: "Greg", score: 98},
-//               {name: "Ari", score: 96},
-//               {name: 'Q', score: 75},
-//               {name: "Loser", score: 48}
-//             ];
- 
-//           scope.$watch(function() {
-//             return angular.element($window)[0].innerWidth;
-//           }, function() {
-//             scope.render(scope.data);
-//           });
- 
-//           scope.$watch('data', function(newData) {
-//             scope.render(newData);
-//           }, true);
- 
-//           scope.render = function(data) {
-//             svg.selectAll('*').remove();
- 
-//             if (!data) return;
-//             if (renderTimeout) clearTimeout(renderTimeout);
- 
-//             renderTimeout = $timeout(function() {
-//               var width = d3.select(ele[0])[0][0].offsetWidth - margin,
-//                   height = scope.data.length * (barHeight + barPadding),
-//                   color = d3.scale.category20(),
-//                   xScale = d3.scale.linear()
-//                     .domain([0, d3.max(data, function(d) {
-//                       return d.score;
-//                     })])
-//                     .range([0, width]);
- 
-//               svg.attr('height', height);
- 
-//               svg.selectAll('rect')
-//                 .data(data)
-//                 .enter()
-//                   .append('rect')
-//                   .on('click', function(d,i) {
-//                     return scope.onClick({item: d});
-//                   })
-//                   .attr('height', barHeight)
-//                   .attr('width', 140)
-//                   .attr('x', Math.round(margin/2))
-//                   .attr('y', function(d,i) {
-//                     return i * (barHeight + barPadding);
-//                   })
-//                   .attr('fill', function(d) {
-//                     return color(d.score);
-//                   })
-//                   .transition()
-//                     .duration(1000)
-//                     .attr('width', function(d) {
-//                       return xScale(d.score);
-//                     });
-//               svg.selectAll('text')
-//                 .data(data)
-//                 .enter()
-//                   .append('text')
-//                   .attr('fill', '#fff')
-//                   .attr('y', function(d,i) {
-//                     return i * (barHeight + barPadding) + 15;
-//                   })
-//                   .attr('x', 15)
-//                   .text(function(d) {
-//                     return d.name + " (scored: " + d.score + ")";
-//                   });
-//             }, 200);
-//           };
-//         };
-//         d3Use(d3);
-//       }}
-// }])
-
-angular.module('donutChart', [])
+angular.module('mean.directives', [])
 .directive('donutChart', function(){
-  function link(scope, el, attr){
+  function link(scope, el, attr){ //scope is the 'model' that the directive is in, second is the element that the directive is one, the third is an object hash with all the element's properties (or attributes)
     console.log('link function returning');
     var color = d3.scale.category10();
     var data = [10, 20, 30];
     var width = 300;
     var height = 300;
     var min = Math.min(width, height);
-    var svg = d3.select(el[0]).append('svg');
+    var svg = d3.select(el[0]).append('svg'); //we use el[0] instead of just el because element is a jQuery-lite wrapped selection and not an ordinary DOM object. 
     var pie = d3.layout.pie().sort(null);
     var arc = d3.svg.arc()
       .outerRadius(min / 2 * 0.9)
@@ -129,9 +26,60 @@ angular.module('donutChart', [])
         .style('stroke', 'white')
         .attr('d', arc)
         .attr('fill', function(d, i){ return color(i) });
+    scope.$watch('data', function(data){
+      console.log('an element within "data" changed!');
+      console.log(data);
+    }, true);
   }
   return {
     link: link,
     restrict: 'E'
   };
 });
+
+//this directive is messing up everything 
+angular.module('mean.directives', [])
+.directive('albersUsa', function(){
+  function link(scope, element, attribute){
+    console.log('albersUsa link function returning');
+    console.log(topojson)
+
+    var width = 960,
+        height = 500;
+
+    var projection = d3.geo.albersUsa()
+        .scale(1000)
+        .translate([width / 2, height / 2]);
+
+    var path = d3.geo.path()
+        .projection(projection);
+
+    var svg = d3.select(element[0]).append("svg")
+        .attr("width", width)
+        .attr("height", height);
+
+    d3.json("/lib/us.json", function(error, us) {//changed this to point to a us.json file in the public directory
+      svg.insert("path", ".graticule")
+          .datum(topojson.feature(us, us.objects.land)) //topojson not defined at this point, for some reason. why? this.topojson is returned by the topojson minified script. why is it not returned? 
+          .attr("class", "land")
+          .attr("d", path);
+
+      svg.insert("path", ".graticule")
+          .datum(topojson.mesh(us, us.objects.counties, function(a, b) { return a !== b && !(a.id / 1000 ^ b.id / 1000); }))
+          .attr("class", "county-boundary")
+          .attr("d", path);
+
+      svg.insert("path", ".graticule")
+          .datum(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; }))
+          .attr("class", "state-boundary")
+          .attr("d", path);
+    });
+
+    d3.select(self.frameElement).style("height", height + "px");
+  }
+  return {
+    link: link, 
+    restrict: 'EA'
+  }
+
+})
