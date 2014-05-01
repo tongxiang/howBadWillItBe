411094a3c39fa4fe129614b14b75282a301c075b
Tong Xiang <tong.xiang2@gmail.com>
2014-04-30 20:59:07 -0400

    albersUsa

--- public/js/directives.js
+++ public/js/directives.js
@@ -1,41 +1,41 @@
 'use strict';
 
-angular.module('mean.directives', [])
-.directive('donutChart', function(){
-  function link(scope, el, attr){ //scope is the 'model' that the directive is in, second is the element that the directive is one, the third is an object hash with all the element's properties (or attributes)
-    console.log('link function returning');
-    var color = d3.scale.category10();
-    var data = [10, 20, 30];
-    var width = 300;
-    var height = 300;
-    var min = Math.min(width, height);
-    var svg = d3.select(el[0]).append('svg'); //we use el[0] instead of just el because element is a jQuery-lite wrapped selection and not an ordinary DOM object. 
-    var pie = d3.layout.pie().sort(null);
-    var arc = d3.svg.arc()
-      .outerRadius(min / 2 * 0.9)
-      .innerRadius(min / 2 * 0.5);
+// angular.module('mean.directives', [])
+// .directive('donutChart', function(){
+//   function link(scope, el, attr){ //scope is the 'model' that the directive is in, second is the element that the directive is one, the third is an object hash with all the element's properties (or attributes)
+//     console.log('link function returning');
+//     var color = d3.scale.category10();
+//     var data = [10, 20, 30];
+//     var width = 300;
+//     var height = 300;
+//     var min = Math.min(width, height);
+//     var svg = d3.select(el[0]).append('svg'); //we use el[0] instead of just el because element is a jQuery-lite wrapped selection and not an ordinary DOM object. 
+//     var pie = d3.layout.pie().sort(null);
+//     var arc = d3.svg.arc()
+//       .outerRadius(min / 2 * 0.9)
+//       .innerRadius(min / 2 * 0.5);
 
-    svg.attr({width: width, height: height});
-    var g = svg.append('g')
-      // center the donut chart
-      .attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')');
+//     svg.attr({width: width, height: height});
+//     var g = svg.append('g')
+//       // center the donut chart
+//       .attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')');
     
-    // add the <path>s for each arc slice
-    g.selectAll('path').data(pie(data))
-      .enter().append('path')
-        .style('stroke', 'white')
-        .attr('d', arc)
-        .attr('fill', function(d, i){ return color(i) });
-    scope.$watch('data', function(data){
-      console.log('an element within "data" changed!');
-      console.log(data);
-    }, true);
-  }
-  return {
-    link: link,
-    restrict: 'E'
-  };
-});
+//     // add the <path>s for each arc slice
+//     g.selectAll('path').data(pie(data))
+//       .enter().append('path')
+//         .style('stroke', 'white')
+//         .attr('d', arc)
+//         .attr('fill', function(d, i){ return color(i) });
+//     scope.$watch('data', function(data){
+//       console.log('an element within "data" changed!');
+//       console.log(data);
+//     }, true);
+//   }
+//   return {
+//     link: link,
+//     restrict: 'E'
+//   };
+// });
 
 //this directive is messing up everything 
 angular.module('mean.directives', [])
